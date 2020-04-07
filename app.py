c322026073653ef4455d9a9bcf1d0b548b38cf1b
rrivera111 <rrodolfo.rivera@gmail.com>
2020-04-07 16:22:06 +0200

    updates3

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 from flask import Flask, render_template, request ,jsonify
 import requests
 import max_reader
-#from bs4 import BeautifulSoup
-
 
 app = Flask(__name__)
-
-#sensor1 = max_reader.pt1000()
-#temp = sensor1.Temp()
-
 
 @app.route('/',methods = ['POST', 'GET'])
 def index():
     sensor1 = max_reader.tempreading1()
-    #temp = sensor1.Temp()
-    #text = temp 
     text = jsonify(sensor1) 
     num = jsonify(sensor1)
     tempVal1 =  sensor1
     return render_template('index.html', **locals())
-
 
 @app.route('/suggestions',methods = ['POST', 'GET'])
 def suggestions():
@@ -32,8 +23,6 @@ def suggestions():
     tempVal1 =  sensor1
     tempVal2 =  sensor2
     return render_template('suggestions.html', **locals())
-    #return render_template('suggestions.html', suggestions=suggestions_list)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
---
+++ mqttPub.py
@@ -0,0 +1,48 @@
+import sys, os 
+import time 
+import paho.mqtt.client as mqtt
+import random, threading, json 
+from datetime import datetime 
+import max_reader
+
+MQTT_Broker = "localhost"
+MQTT_Port = 1883 
+Keep_Alive_Interval = 45 
+MQTT_Topic_Boiler = "Boiler" 
+MQTT_Topic_Heater = "Heater"
+
+mqttc = mqtt.Client() 
+mqttc.connect(MQTT_Broker, int(MQTT_Port),int(Keep_Alive_Interval))
+toggle = 0 
+# PT 1000 definition 
+
+sensor1 = max_reader.tempreading1()
+sensor2 = max_reader.tempreading2()
+
+# Functions 
+# Publush to topic 
+def publish_To_Topic(topic, message):
+	mqttc.publish(topic,message)
+
+# Gathers sensor data and publush to defined topics 
+def publish_Fake_Sensor_Values_to_MQTT(): 
+	threading.Timer(3.0, publish_Fake_Sensor_Values_to_MQTT).start()
+	global toggle
+	if toggle == 0:
+		valueSensor1 = float("{0:.2f}".format(sensor1))
+		valueSensor1_Data = {}
+		valueSensor1_json_data = json.dumps(valueSensor1_Data)
+		publish_To_Topic (MQTT_Topic_Boiler, valueSensor1)
+		toggle = 1
+	else:
+		valueSensor2 = float("{0:.2f}".format(sensor2))
+		valueSensor2_Data = {}
+		valueSensor2_json_data = json.dumps(valueSensor2_Data)
+		#print str(temperature_json_data) 
+		publish_To_Topic (MQTT_Topic_Heater, valueSensor2)
+		toggle = 0
+
+
+publish_Fake_Sensor_Values_to_MQTT()
+
+
