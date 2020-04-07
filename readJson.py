b91baf495cac24c2e93cbd838dfd3ceef7f0c52e
rrivera111 <rrodolfo.rivera@gmail.com>
2020-04-07 20:13:00 +0200

    test

--- readJson.py
+++ readJson.py
@@ -6,13 +6,12 @@ def readSettings ():
     #setpoint1 = data['setpoint1']
     #setpoint2 = data['setpoint2']
     #deltaT    = data['deltaT']
-    x1 = data['x1']
-    x2 = data['x2']
-    x3 = data['x3']
-
-  return(x1,x2,x3)
-
-
+    #x1 = data['x1']
+    #x2 = data['x2']
+    #x3 = data['x3']
+  return data
+  #return(x1,x2,x3)
+  
 read = readSettings() 
 
 print read 
