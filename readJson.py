b80eb5c68bb87824b36b2538011a3ec81d966964
rrivera111 <rrodolfo.rivera@gmail.com>
2020-04-07 18:50:19 +0000

    test

--- readJson.py
+++ readJson.py
@@ -1,19 +1,24 @@
 import json
+#import pandas as pd
 
 def readSettings ():
   with open('settings.json') as f:
     data = json.load(f)
+    print(type(data))
+    df = pd.DataFrame(data)
+    print(type(df))
     #setpoint1 = data['setpoint1']
     #setpoint2 = data['setpoint2']
     #deltaT    = data['deltaT']
-    x1 = data["x1"]
-    x2 = data["x2"]
-    x3 = data["x3"]
-  #return data
-  return(x1,x2,x3)
+    x1 = data[0]
+    #x2 = data[1]
+   # x3 = data[2]
+  return df
+  #return(x1,x2,x3)
   
 read = readSettings() 
 
-print read 
+print (read)
+print (type(read))
 
 
