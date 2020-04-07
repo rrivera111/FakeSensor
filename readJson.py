f350a6c627f2d451281a54775b165e68822ca7a5
rrivera111 <rrodolfo.rivera@gmail.com>
2020-04-07 19:53:17 +0200

    updated

--- readJson.py
+++ readJson.py
@@ -3,11 +3,18 @@ import json
 def readSettings ():
   with open('settings.json') as f:
     data = json.load(f)
-    setpoint1 = data['setpoint1']
-    setpoint2 = data['setpoint2']
-    deltaT    = data['deltaT']
-  return(setpoint1,setpoint2,deltaT)
+    #setpoint1 = data['setpoint1']
+    #setpoint2 = data['setpoint2']
+    #deltaT    = data['deltaT']
+    x1 = data['x1']
+    x2 = data['x2']
+    x3 = data['x3']
+
+  return(x1,x2,x3)
 
 
+read = readSettings() 
+
+print rread 
 
 
