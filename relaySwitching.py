cab89b0395760b3b37b6c75dcbea740e52c9d760
rrivera111 <rrodolfo.rivera@gmail.com>
2020-04-10 01:46:54 +0200

    test

--- relaySwitching.py
+++ relaySwitching.py
@@ -9,18 +9,19 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 # GPIO.setwarnings(False)
 # relays definition 
-RelayList = [23,22,4,27,12,17,18,20]
+#RelayList = [23,22,4,27,12,17,18,20]
+RelayList = [5, 6, 13, 16, 19, 20, 21, 26] # Original distribution board
 # GPIO pins defined as ouputs
 GPIO.setup(RelayList, GPIO.OUT)
 class relay:
-	def __init__(self, pin, invert=False, init=False):
+	def __init__(self, pin) #, invert=False, init=False):
 		self._pin = pin
-		self.inverted = invert
-		self.state = init
-		GPIO.setup(self._pin, GPIO.OUT,initial=init^invert) # 
+		#self.inverted = invert
+		#self.state = init
+		GPIO.setup(self._pin, GPIO.OUT) #,initial=init^invert) # 
 	def set_output(self,state):
 		self.state = bool(state)
-		GPIO.output(self._pin,self.state^self.inverted)
+		GPIO.output(self._pin,self.state)
 	def on(self):
 		self.set_output(True)
 	def off(self):
@@ -37,13 +38,15 @@ class relay:
 if __name__ == '__main__':
     print(len(RelayList))
     #Relay.OFF(23)
-    for i in RelayList :
+    for i in range(0,2) :
         testRelay = relay(RelayList[i])
+        testRelay.on()
+        time.slepp(0.5)
+        #testRelay = relay(RelayList[i])
+        testRelay.off()
         print(RelayList[i])
     #    testRelay.ON(RelayList[i])
     #    time.sleep(.5)
     #for i in RelayList : 
     #    testRelay.OFF(RelayList[i])
     #    time.sleep(.5)
-
-    time.sleep(1)
